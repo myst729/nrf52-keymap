bd71884ecdb2d8431ca4001205c02e26ff01f948
Leo Deng <myst.dg@gmail.com>
2024-10-14 18:29:59 +0800

    dg68v5

--- README.md
+++ README.md
@@ -1,5 +1,42 @@
-# lot42
+# LXVIII
+
+68-keys 65% keyboard.
+
+## Build
 
 ```shell
 make go
 ```
+
+
+## Wiring
+
+```
+       C0     C1    C2    C3    C4    C5     C6    C7    C8    C9    C10   C11    C12       C13       C14      C15
+   ,-------+-----+-----+-----+-----+-----+-------+-----+-----+-----+-----+-----+-------+-----------+-------+----------.
+R0 |  Esc  |  1  |  2  |  3  |  4  |  5  |   6   |  7  |  8  |  9  |  0  |  -  |   =   | Backspace |       |    `     |
+   |-------+-----+-----+-----+-----+-----+-------+-----+-----+-----+-----+-----+-------+-----------+-------+----------|
+R1 |  Tab  |     |  Q  |  W  |  E  |  R  |   T   |  Y  |  U  |  I  |  O  |  P  |   [   |     ]     |   \   |  Delete  |
+   |-------+-----+-----+-----+-----+-----+-------+-----+-----+-----+-----+-----+-------+-----------+-------+----------|
+R2 | Caps  |     |  A  |  S  |  D  |  F  |   G   |  H  |  J  |  K  |  L  |  ;  |   '   |           | Enter |  PageUp  |
+   |-------+-----+-----+-----+-----+-----+-------+-----+-----+-----+-----+-----+-------+-----------+-------+----------|
+R3 | Shift |     |  Z  |  X  |  C  |  V  |   B   |  N  |  M  |  ,  |  .  |  /  | Shift |           |  Up   | PageDown |
+   |-------+-----+-----+-----+-----+-----+-------+-----+-----+-----+-----+-----+-------+-----------+-------+----------|
+R4 | Ctrl  |     | GUI | Alt |     |     | Space |     |     |     | Alt | Fn  | Ctrl  |   Left    | Down  |  Right   |
+   `-------+-----+-----+-----+-----+-----+-------+-----+-----+-----+-----+-----+-------+-----------+-------+----------'
+```
+
+
+## Hotkeys
+
+- <kbd>F</kbd> + <kbd>J</kbd>                          开机/唤醒
+- <kbd>LShift</kbd> + <kbd>RShift</kbd> + <kbd>H</kbd> 输出剩余电量
+- <kbd>LShift</kbd> + <kbd>RShift</kbd> + <kbd>P</kbd> 待机/睡眠
+- <kbd>LShift</kbd> + <kbd>RShift</kbd> + <kbd>M</kbd> 切换有线/蓝牙
+- <kbd>LShift</kbd> + <kbd>RShift</kbd> + <kbd>Q</kbd> 切换蓝牙通道 1
+- <kbd>LShift</kbd> + <kbd>RShift</kbd> + <kbd>W</kbd> 切换蓝牙通道 2
+- <kbd>LShift</kbd> + <kbd>RShift</kbd> + <kbd>E</kbd> 切换蓝牙通道 3
+- <kbd>LShift</kbd> + <kbd>RShift</kbd> + <kbd>R</kbd> 开启蓝牙广播
+- <kbd>LShift</kbd> + <kbd>RShift</kbd> + <kbd>B</kbd> 进入蓝牙刷机模式
+- <kbd>LShift</kbd> + <kbd>RShift</kbd> + <kbd>O</kbd> 清空绑定数据
+- <kbd>LShift</kbd> + <kbd>RShift</kbd> + <kbd>I</kbd> 重置键盘
--- keymap_plain.c
+++ keymap_plain.c
@@ -3,28 +3,31 @@
 
 const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KEYMAP(
-    KC_WH_D, KC_WH_U,          KC_FN0,  KC_HOME, KC_END,
-    KC_E,    KC_R,    KC_T,    KC_U,    KC_I,    KC_P,
-    KC_D,    KC_G,    KC_J,    KC_B,    KC_ESC,  KC_ENT,
-    KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,
-    KC_LCTL,          KC_LALT, KC_SPC,           KC_M),
+    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          KC_GRV,
+    KC_TAB,           KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL,
+    KC_CAPS,          KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP,
+    KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_PGDN,
+    KC_LCTL,          KC_LALT, KC_LGUI,                   KC_SPC,                             KC_RGUI, KC_RALT, KC_FN0,  KC_LEFT, KC_DOWN, KC_RGHT),
 
   KEYMAP(
-    KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_FN1,  KC_FN2,  KC_FN3,  KC_FN4,  KC_FN5,  KC_FN6,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_FN8,           KC_TRNS, KC_TRNS,          KC_FN7)
+    KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS,          KC_TRNS,
+    KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PSCR, KC_SLCK, KC_PAUS, KC_TRNS, KC_INS,
+    KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_BRTD, KC_BRTI,          KC_TRNS, KC_HOME,
+    KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MUTE, KC_VOLD, KC_VOLU, KC_TRNS,          KC_TRNS, KC_END,
+    KC_TRNS,          KC_TRNS, KC_TRNS,                   KC_TRNS,                            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
 };
 
 const action_t fn_actions[] = {
   ACTION_LAYER_MOMENTARY(1),
-  ACTION_FUNCTION_OPT(SWITCH_DEVICE, SWITCH_DEVICE_USB),
-  ACTION_FUNCTION_OPT(SWITCH_DEVICE, SWITCH_DEVICE_BLE_0),
-  ACTION_FUNCTION_OPT(SWITCH_DEVICE, SWITCH_DEVICE_BLE_1),
-  ACTION_FUNCTION_OPT(SWITCH_DEVICE, SWITCH_DEVICE_BLE_2),
-  ACTION_FUNCTION_OPT(SWITCH_DEVICE, SWITCH_DEVICE_BLE_REBOND),
-  ACTION_FUNCTION_OPT(KEYBOARD_CONTROL, CONTROL_BATTERY_PERCENTAGE),
-  ACTION_FUNCTION_OPT(KEYBOARD_CONTROL, CONTROL_SLEEP),
-  ACTION_FUNCTION_OPT(KEYBOARD_CONTROL, CONTROL_BOOTLOADER),
+  // F + J                开机/唤醒
+  // LShift + RShift + H  输出剩余电量
+  // LShift + RShift + P  待机/睡眠
+  // LShift + RShift + M  切换有线/蓝牙
+  // LShift + RShift + Q  切换蓝牙通道1
+  // LShift + RShift + W  切换蓝牙通道2
+  // LShift + RShift + E  切换蓝牙通道3
+  // LShift + RShift + R  开启蓝牙广播
+  // LShift + RShift + B  进入蓝牙刷机模式
+  // LShift + RShift + O  清空绑定数据
+  // LShift + RShift + I  重置键盘
 };
