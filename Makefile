b5c900801930fe4fdbf8291f618e7413252966c3
Leo Deng <myst.dg@gmail.com>
2024-11-11 09:50:22 +0800

    dg68v5win

--- Makefile
+++ Makefile
@@ -1,6 +1,9 @@
 # 此工程的根目录
 ROOT_DIR := ../..
 
+# 主控芯片类型：可选 nrf52810 或 nrf52832
+NRF_CHIP := nrf52832
+
 # 特殊目录控制
 SDK_ROOT := $(ROOT_DIR)/SDK
 TEMPLATE_PATH := $(ROOT_DIR)/template
@@ -14,9 +17,22 @@ INC_FOLDERS += .
 # 配置文件文件名
 CONFIG_H = config.h
 CONFIG_H_DIR = .
-SRC_FILES += keymap_plain.c # user_evt.c
+SRC_FILES += keymap_plain.c
 
-include ./rules.mk
+# 功能选项：更多可配置项目，请参考doc目录下的相应文档
+BOOTMAGIC_ENABLE = no   # 启用Bootmagic
+BOOTCHECK_ENABLE = yes  # 启用Bootcheck
+EXTRAKEY_ENABLE = yes   # 启用媒体键功能
+MOUSEKEY_ENABLE = no    # 启用鼠标键功能
+USB_6KRO_ENABLE = yes   # 启用USB的六键无冲功能
+NKRO_ENABLE = yes       # 启用USB的全键无冲功能
+COMMAND_ENABLE = yes    # 启用调试和配置的命令
+RGBLIGHT_ENABLE = no    # 启用RGB灯带
+ONE_RGB_LED = no        # 启用RGB指示灯
+THREE_LED_STATUS = no   # 启用键盘运行状态灯
+LEDMAP = no             # 启用可配置LED状态灯
+ONBOARD_CMSIS_DAP = yes # 启用板载调试器
+NRF52_DISABLE_FPU = yes # 禁用FPU
 
 purge:
 	-rm -rf $(OUTPUT_DIR)
--- keymap_plain.c
+++ keymap_plain.c
@@ -3,61 +3,22 @@
 
 const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KEYMAP(
-    KC_ESC,           KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,           KC_PSCR, KC_SLCK, KC_PAUS,
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          KC_INS,  KC_HOME,  KC_PGUP,
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          KC_DEL,  KC_END,   KC_PGDN,
-    KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
-    KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,                   KC_UP,
-    KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_FN0,  KC_RALT, KC_MENU, KC_RCTL,          KC_LEFT, KC_DOWN, KC_RGHT),
+    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          KC_GRV,
+    KC_TAB,           KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL,
+    KC_CAPS,          KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP,
+    KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_PGDN,
+    KC_LCTL,          KC_LGUI, KC_LALT,                   KC_SPC,                             KC_RGUI, KC_RALT, KC_FN0,  KC_LEFT, KC_DOWN, KC_RGHT),
 
   KEYMAP(
-    KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_FN1,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_FN2,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,
-    KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,                   KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,                            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS),
-
+    KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS,          KC_TRNS,
+    KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PSCR, KC_SLCK, KC_PAUS, KC_TRNS, KC_INS,
+    KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_BRTD, KC_BRTI,          KC_TRNS, KC_HOME,
+    KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MUTE, KC_VOLD, KC_VOLU, KC_TRNS,          KC_TRNS, KC_END,
+    KC_TRNS,          KC_TRNS, KC_TRNS,                   KC_TRNS,                            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
 };
 
 const action_t fn_actions[] = {
-  /* Poker Layout */
-  ACTION_LAYER_MOMENTARY(1),         // to Fn overlay
-  ACTION_FUNCTION(SWITCH_DEVICE),    // switch between usb and ble
-  ACTION_FUNCTION(KEYBOARD_CONTROL), // sleep
-
-  /*
   ACTION_LAYER_MOMENTARY(1),
-  ACTION_LAYER_TOGGLE(2),
-  ACTION_LAYER_TAP_KEY(3, KC_SPC),
-  ACTION_FUNCTION_OPT(KEYBOARD_CONTROL, CONTROL_BOOTLOADER),
-  ACTION_FUNCTION_OPT(KEYBOARD_CONTROL, CONTROL_SLEEP),
-  ACTION_FUNCTION_OPT(KEYBOARD_CONTROL, CONTROL_NKRO),
-  ACTION_FUNCTION_OPT(KEYBOARD_CONTROL, CONTROL_BATTERY_PERCENTAGE),
-  ACTION_FUNCTION_OPT(SWITCH_DEVICE, SWITCH_DEVICE_USB),
-  ACTION_FUNCTION_OPT(SWITCH_DEVICE, SWITCH_DEVICE_BLE_0),
-  ACTION_FUNCTION_OPT(SWITCH_DEVICE, SWITCH_DEVICE_BLE_1),
-  ACTION_FUNCTION_OPT(SWITCH_DEVICE, SWITCH_DEVICE_BLE_2),
-  ACTION_FUNCTION_OPT(SWITCH_DEVICE, SWITCH_DEVICE_BLE_REBOND),
-  ACTION_MODS_KEY(MOD_LSFT, KC_GRV),   // ~
-  ACTION_MODS_KEY(MOD_LSFT, KC_1),     // !
-  ACTION_MODS_KEY(MOD_LSFT, KC_2),     // @
-  ACTION_MODS_KEY(MOD_LSFT, KC_3),     // #
-  ACTION_MODS_KEY(MOD_LSFT, KC_4),     // $
-  ACTION_MODS_KEY(MOD_LSFT, KC_5),     // %
-  ACTION_MODS_KEY(MOD_LSFT, KC_6),     // ^
-  ACTION_MODS_KEY(MOD_LSFT, KC_7),     // &
-  ACTION_MODS_KEY(MOD_LSFT, KC_8),     // *
-  ACTION_MODS_KEY(MOD_LSFT, KC_9),     // (
-  ACTION_MODS_KEY(MOD_LSFT, KC_0),     // )
-  ACTION_MODS_KEY(MOD_LSFT, KC_MINS),  // _
-  ACTION_MODS_KEY(MOD_LSFT, KC_EQL),   // +
-  ACTION_MODS_KEY(MOD_LSFT, KC_LBRC),  // {
-  ACTION_MODS_KEY(MOD_LSFT, KC_RBRC),  // }
-  ACTION_MODS_KEY(MOD_LSFT, KC_QUOT),  // "
-  ACTION_MODS_KEY(MOD_LSFT, KC_BSLS),  // |
-  */
-
   // F + J                开机/唤醒
   // LShift + RShift + H  输出剩余电量
   // LShift + RShift + P  待机/睡眠
